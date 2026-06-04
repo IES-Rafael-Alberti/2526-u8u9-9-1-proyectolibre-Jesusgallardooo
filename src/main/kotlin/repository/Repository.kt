7a69def5da25d52ad29a3a96880f190bbbb6e337
Jesusgallardooo <jgallardo@iesrafaelalberti.es>
2026-06-04 16:13:35 +0200

    interfaz repository generica

--- src/main/kotlin/repository/Repository.kt
+++ src/main/kotlin/repository/Repository.kt
@@ -1,8 +1,9 @@
 package repository
 
-interface Repository<T> {
+interface Repository<T, ID> {
     fun findAll(): List<T>
-    fun findById(id: Long): T?
-    fun save(entity: T)
-    fun delete(id: Long)
+    fun findById(id: ID): T?
+    fun update(entity: T): T?
+    fun create(entity: T)
+    fun delete(id: ID): Boolean
 }
